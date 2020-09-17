00700c48221cfc4449a5c7b8174e196ea44de815
brian <brianra2nil@hotmail.com>
2020-09-17 13:42:23 -0700

    3 pages work and linked

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
-import Login from './pages/Portfolio'
-import Login from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
