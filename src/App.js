1a5c1433c396faaf311ca3a9494e05a84fdfb158
brian <brianra2nil@hotmail.com>
2020-09-17 13:40:03 -0700

    setup 3 pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Portfolio'
+import Login from './pages/Contact'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Portfolio">Portfolio</Link>
+          <Link to="/Contact">Contact</Link>
+        </nav>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/Portfolio" component={Portfolio} />
+        <Route path="/Contact" component={Contact} />
+      </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
