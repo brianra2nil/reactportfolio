eb426bcae121f5bd43197444b83683b248f46dfd
brian <brianra2nil@hotmail.com>
2020-09-23 21:01:18 -0700

    updated contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Jumbotron, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
   return (
-    <h1>This is the Contact page</h1>
+    <div>
+    <Jumbotron>
+      <h1 className="display-3">Contact Info</h1>
+      <p className="lead">Brian Ratunil, Phone:  951-837-6791</p>
+      <hr className="my-2" />
+      <p>You may reach me via phone or email</p>
+      <p className="lead"></p>
+      
+      <Link to="mailto:brianra2nil@hotmail.com" > Email Me </Link> 
+    
+    </Jumbotron>
+  </div>
   )
 }
 
